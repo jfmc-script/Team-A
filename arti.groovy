6ecd97a37c151854224e7af1fd7bb8797cbc9fa2
mission-control <mission-control@mission-control-2020005603-7tbs7>
2017-10-13 23:05:31 +0000

    File changed by admin

--- arti.groovy
+++ arti.groovy
@@ -1,6 +1,42 @@
-artifactory("Art-1") {
-  localRepository("docker-local-a") {
-    packageType "docker"
-    description "My local docker registry-vvvvvv"
-  }
+team = userInput (
+        type : "STRING", // "BOOLEAN", "INTEGER", "INSTANCE", "REPOSITORY"
+        description : "Repository Key",
+        validations : (["cron"])
+)
+packageTypeUserInput = "generic"
+/* userInput (
+name : "Input name Package Type",
+type : "PACKAGE_TYPE",
+value : "",
+description : "please provide a value"
+) */
+//construct a namespace
+//team-technology-maturity-location
+def maturitylist = ["dev", "prod"]
+//def locationslist = ["sunnyvale", "shanghai"]
+localname = ""
+//virtname = ""
+//def virtualrepos = []
+def localrepos = []
+def namecreator(String maturity, String location)
+{ //this function creates a list of all repos
+    localname = team + "-" + packageTypeUserInput + "-" + maturity + "-" + location
+    return localname
+    //localrepos.add (localname)
+}
+//virtname = team + "-" + packageTypeUserInput + "-" + maturity
+//virtualrepos.add(virtname)
+//}
+//}
+def SERVICE_NAME = 'service-name'
+def service = services[SERVICE_NAME]
+for (localservice in service) {
+    artifactory(localservice.id)
+            {
+                localRepository(createRepoKey(namecreator(prod,local))) {
+                    description "Public Description"
+                    notes "Some internal notes"
+                    packageType packageTypeUserInput // "maven" | "gradle" | "ivy" | "sbt" | "nuget" | "gems" | "npm" | "bower" | "debian" | "pypi" | "docker" | "vagrant" | "gitlfs" | "yum" | "generic"
+                }
+            }
 }
