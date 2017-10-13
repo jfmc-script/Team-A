a4e6f7983ddbd6961f733cc575134f10ef91cb02
mission-control <mission-control@mission-control-2020005603-7tbs7>
2017-10-13 23:14:07 +0000

    File changed by admin

--- arti.groovy
+++ arti.groovy
@@ -30,7 +30,7 @@ def namecreator(String maturity, String location)
 //}
 def localservice=services["Art-2"]
 //for (localservice in service) {
-    artifactory(localservice.id)
+    artifactory(localservice)
             {
                 localRepository("random-generic-prod-local"/*namecreator("prod","local")*/) {
                     description "Public Description"
