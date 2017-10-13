74d6fa8428a79076852f8e9ffe4e05509e675ffc
mission-control <mission-control@mission-control-2020005603-7tbs7>
2017-10-13 20:05:58 +0000

    File changed by admin

--- arti.groovy
+++ arti.groovy
@@ -1,6 +1,6 @@
 artifactory("Art-1") {
   localRepository("mvn-local-a") {
     packageType "maven"
-    description "My local Docker registry-vvvvvv"
+    description "My local maven registry-vvvvvv"
   }
 }
