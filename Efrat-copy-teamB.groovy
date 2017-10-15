1ccd75c82969c6428ce85e8ca6f4abd93c0eee8c
jfmc-script <32617524+jfmc-script@users.noreply.github.com>
2017-10-15 14:51:19 +0300

    Create Efrat-copy-teamB.groovy

---
+++ Efrat-copy-teamB.groovy
@@ -0,0 +1,13 @@
+// ask user for repository to replicate
+repo = userInput(name: "Repository to replicate:", type: "REPOSITORY")
+// ask user for instances to replicate to
+other = userInput(name: "Instances to replicate to:", type: "ARTIFACTORY", multivalued: true)
+
+for (art in other) {
+  artifactory(art.name) {
+      localRepository('repo-local') {
+        description 'description'
+        packageType 'generic'
+      }
+  }
+}
